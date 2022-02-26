5f02fcfc748145f41c9b3fa35f57c658da05e2f7
Nurul Ismat Tanni <57992438+ismat-tanni@users.noreply.github.com>
2022-02-27 00:08:13 +0800

    Add files via upload

---
+++ tutorial2.2(self)/bin/main.dart
@@ -0,0 +1,54 @@
+// SCSJ3623 Mobile Application Programming
+// Tutorial 2.2
+
+// Date: _________
+// Section: _________
+// Room Number: _________
+
+// Member 1's Name: _____________    Location: ____________ (i.e. where are you currently located)
+// Member 2's Name: _____________    Location: ____________
+// Member 3's Name: _____________    Location: ____________
+// Member 4's Name: _____________    Location: ____________
+
+// Task 1a: Define class Person
+
+//  Task 1b: Define class Result as a mixin
+
+//  Task 1c: Define class Student
+
+void main() {
+  // Task 2: Store the input data in a multi-line string variable
+
+  // Task 3: Split the text into a list of single line string
+
+  // Task 4: create a list of Student objects from the strings
+
+  // Task 5: Print the list of students with their scores, grades and other info
+  print('Name\t\tYear\t\tMatric\t\tScore\tGrade');
+  print('----\t\t----\t\t------\t\t------\t----');
+
+  print('Average score: ');
+  print('Average grade: ');
+}
+
+// Resources
+// 1.	How do I call a super constructor in Dart?
+//    https://stackoverflow.com/questions/13272035/how-do-i-call-a-super-constructor-in-dart
+
+// 2.	substring method
+//    https://api.flutter.dev/flutter/dart-core/String/substring.html
+
+// 3.	How do I parse a string into a number with Dart?
+//    https://stackoverflow.com/questions/13167496/how-do-i-parse-a-string-into-a-number-with-dart
+
+// 4.	Multi-lines string in Dart
+//    https://stackoverflow.com/questions/13104729/multi-lines-string-in-darttext
+
+// 5.	split method
+//    https://api.flutter.dev/flutter/dart-core/String/split.htmltext
+
+// 6.	Top 10 Array utility methods you should know (Dart)
+//    https://codeburst.io/top-10-array-utility-methods-you-should-know-dart-feb2648ee3a2
+
+// 7.	round method
+//    https://api.dart.dev/stable/2.7.1/dart-core/num/round.html
