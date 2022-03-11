d6cc8f5b671071547552f02d546fcbc71deb78af
Nurul Ismat Tanni <57992438+ismat-tanni@users.noreply.github.com>
2022-03-11 14:56:47 +0800

    Delete navigation_simple-master directory

--- navigation_simple-master/lib/models/group_member.dart
+++
@@ -1,7 +0,0 @@
-class GroupMember {
-  String shortName;
-  String fullName;
-
-  GroupMember(this.shortName, this.fullName);
-  GroupMember.copy(GroupMember from) : this(from.shortName, from.fullName);
-}
